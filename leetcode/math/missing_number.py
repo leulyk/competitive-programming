d6a5fa25e0d5ddfc6abca0aeb29b541ac20b032a
Leul Yiheyis <leulykpro@gmail.com>
2024-02-09 16:24:59 +0300

    solve 'missing number' problem

---
+++ leetcode/math/missing_number.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(0, len(nums) + 1)) - sum(nums)
