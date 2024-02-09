f177da8339af199c994a979d3fd1bcf5da694995
Leul Yiheyis <leulykpro@gmail.com>
2024-02-09 17:33:33 +0300

    optimize solution using sets

--- leetcode/hash_tables/find_players_with_zero_or_one_losses.py
+++ leetcode/hash_tables/find_players_with_zero_or_one_losses.py
@@ -23,3 +23,22 @@ class Solution:
         winners[1].sort()
 
         return winners
+
+    def findWinnersOptimized(self, matches: List[List[int]]) -> List[List[int]]:
+        no_loss = set()
+        one_loss = set()
+        multiple_losses = set()
+
+        for match in matches:
+            if match[1] in one_loss:
+                one_loss.remove(match[1])
+                multiple_losses.add(match[1])
+            elif match[1] not in multiple_losses:
+                one_loss.add(match[1])
+                no_loss.discard(match[1])
+
+            if match[0] not in one_loss and match[0] not in multiple_losses:
+                no_loss.add(match[0])
+
+        return [sorted(list(no_loss)), sorted(list(one_loss))]
+
