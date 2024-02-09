d99e9021db7cf24f1c5637a2ffa81b0977115962
Leul Yiheyis <leulykpro@gmail.com>
2024-02-09 16:26:20 +0300

    add additional implementation

--- leetcode/math/missing_number.py
+++ leetcode/math/missing_number.py
@@ -1,5 +1,14 @@
 # https://leetcode.com/problems/missing-number/description/
 
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumber1(self, nums: List[int]) -> int:
         return sum(range(0, len(nums) + 1)) - sum(nums)
+
+    def missingNumber2(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for index in range(len(nums)):
+            if nums[index] != index:
+                return index
+
+        return len(nums)
