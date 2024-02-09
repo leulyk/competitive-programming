31c45f2ae9b08ca382092404571d57f9af764b78
Leul Yiheyis <leulykpro@gmail.com>
2024-02-09 14:39:33 +0300

    solve 'contains duplicate' problem

---
+++ leetcode/sets/contains_duplicate.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
