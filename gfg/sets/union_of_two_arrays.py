e0a35dd1b028afad171a5fa5032236f30bdd69fb
Leul Yiheyis <leulykpro@gmail.com>
2024-02-09 14:45:19 +0300

    solve 'union of two arrays ' problem

---
+++ gfg/sets/union_of_two_arrays.py
@@ -0,0 +1,24 @@
+# https://www.geeksforgeeks.org/problems/union-of-two-arrays3538/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        return len(set(a).union(b))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
