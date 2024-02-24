d5912a9d5222adb1e279c3b8590fadc39e8aa758
Leul Yiheyis <leulykpro@gmail.com>
2024-02-24 10:44:29 +0300

    solve 'transpose matrix' problem

---
+++ leetcode/arrays/transpose_matrix.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/transpose-matrix/description/
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        row = len(matrix)
+        col = len(matrix[0])
+        return [[matrix[j][i] for j in range(row)] for i in range(col)]
